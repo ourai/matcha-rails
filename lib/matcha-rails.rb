c91518a6633154f0774968ae5500722a1e735da8
Ourai Lin <ourairyu@hotmail.com>
2014-11-24 22:45:00 +0800

    加入预编译处理

--- lib/matcha-rails.rb
+++ lib/matcha-rails.rb
@@ -1,4 +1,63 @@
 require 'matcha-rails/version'
 
 module Matcha
+  class << self
+    # Inspired by Kaminari
+    def load!
+      register_compass_extension if compass?
+      register_rails_engine      if rails?
+    end
+
+    # Paths
+    def gem_path
+      @gem_path ||= File.expand_path '..', File.dirname(__FILE__)
+    end
+
+    def stylesheets_path
+      File.join assets_path, 'stylesheets'
+    end
+
+    def fonts_path
+      File.join assets_path, 'fonts'
+    end
+
+    def javascripts_path
+      File.join assets_path, 'javascripts'
+    end
+
+    def assets_path
+      @assets_path ||= File.join gem_path, 'app/assets'
+    end
+
+    # Environment detection helpers
+    def asset_pipeline?
+      defined?(::Sprockets)
+    end
+
+    def compass?
+      defined?(::Compass)
+    end
+
+    def rails?
+      defined?(::Rails)
+    end
+
+    private
+
+    def register_compass_extension
+      ::Compass::Frameworks.register(
+          'matcha',
+          :version               => Matcha::VERSION,
+          :path                  => gem_path,
+          :stylesheets_directory => stylesheets_path,
+          :templates_directory   => File.join(gem_path, 'templates')
+      )
+    end
+
+    def register_rails_engine
+      require 'matcha-rails/engine'
+    end
+  end
 end
+
+Matcha.load!
